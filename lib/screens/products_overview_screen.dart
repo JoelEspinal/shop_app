0e2c4c1465e3ce98949e452b28d4173859157068
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-04-28 20:28:13 +0000

    Use productsGrid but

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+import '../widgets/products_grid.dart';
 
+class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: 
-    );
+        appBar: AppBar(
+          title: Text('MyShop'),
+        ),
+        body: ProductGrid(loadedProducts: null));
   }
 }
-
