068464cbf627a329a80996def50f55cc2e08e645
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-04-30 21:56:21 +0000

    Use product provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
+import '../providers/product.dart';
 import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  ProductItem(this.id, this.title, this.imageUrl);
+  // ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return GestureDetector(
       onTap: () {
         Navigator.of(context).pushNamed(
           ProductDetailScreen.routeName,
-          arguments: id,
+          arguments: product.id,
         );
       },
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: GridTile(
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
           footer: GridTileBar(
@@ -33,7 +37,7 @@ class ProductItem extends StatelessWidget {
               onPressed: () {},
             ),
             title: Text(
-              title,
+              product.title,
               textAlign: TextAlign.center,
             ),
             trailing: IconButton(
