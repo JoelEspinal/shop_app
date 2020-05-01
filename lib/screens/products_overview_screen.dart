1705f2706675cc6a509a3a866555224d790cd10b
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-05-01 20:22:10 +0000

    Change to stateful widget

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,13 +2,42 @@ import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FavoriteOption { Favorites, All }
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  bool _showOnlyFavorites = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('MyShop'),
+          actions: <Widget>[
+            PopupMenuButton(
+                onSelected: (FavoriteOption selectedValue) {
+                  if (selectedValue == FavoriteOption.Favorites) {
+                    _showOnlyFavorites = true;
+                  } else {
+                    _showOnlyFavorites = false;
+                  }
+                },
+                icon: Icon(Icons.more_vert),
+                itemBuilder: (_) => [
+                      PopupMenuItem(
+                        child: Text('Only Favorites'),
+                        value: FavoriteOption.Favorites,
+                      ),
+                      PopupMenuItem(
+                        child: Text('Show All'),
+                        value: FavoriteOption.All,
+                      ),
+                    ]),
+          ],
         ),
-        body: ProductGrid());
+        body: ProductGrid(_showOnlyFavorites));
   }
 }
