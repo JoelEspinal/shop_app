7424867fbf6c84a3e9fb3d65fae22af696a61bb6
Joel Espinal <j.espinalcarrasco@gmail.com>
2020-05-02 10:42:00 +0000

    navigate to cart screen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import '../providers/cart.dart';
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
+import '../screens/cart_screen.dart';
 
 enum FavoriteOption { Favorites, All }
 
@@ -50,7 +51,9 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
               ),
               child: IconButton(
                 icon: Icon(Icons.shopping_cart),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.of(context).pushNamed(CartScreen.routeName);
+                },
               ),
             ),
           ],
